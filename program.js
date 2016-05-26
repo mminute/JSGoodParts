64cfef25ffe4e5d96c55cd54423b3088a9bed8c4
mminute <masondjennings@gmail.com>
2016-05-25 22:21:31 -0700

    Adds .pages document

--- program.js
+++ program.js
@@ -1,9 +1,9 @@
 document.writeln('Hello World');
 
-// Function.prototype.method = function (name, func) {
-//   this.prototype[name] = func;
-//   return this;
-// };
+Function.prototype.method = function (name, func) {
+  this.prototype[name] = func;
+  return this;
+};
 
 
 // ========================================
@@ -429,7 +429,7 @@ document.writeln('Currying: ' + add1(6));
 // ===================
 // Memoization
 // ===================
-document.writeln('');
+document.writeln();
 
 var fibCalls = 0;
 
@@ -463,7 +463,7 @@ var fibonacciMemoized = (function() {
   return fib;
 }());
 
-document.writeln('');
+document.writeln();
 
 for(var i=0 ; i <= 10; i += 1) {
   document.writeln('//' + i + ': ' + fibonacciMemoized(i));
@@ -485,7 +485,7 @@ var memoizer = function(memo, formula) {
   return recur;
 };
 
-document.writeln('');
+document.writeln();
 document.writeln('Fibonacci built with memoizer');
 fibCalls = 0;
 
@@ -499,7 +499,7 @@ for(var i=0 ; i <= 10; i += 1) {
 document.writeln('Called fibonacciBuiltWithMemoizer ' + fibCalls + ' times.');
 
 
-document.writeln('');
+document.writeln();
 document.writeln('Factorial built with memoizer');
 fibCalls = 0;
 
@@ -576,7 +576,7 @@ var says = myCat.says(); // meow
 var purr = myCat.purr(5); // 'r-r-r-r-r'
 var name = myCat.get_name(); // 'meow Henrietta meow'
 
-document.writeln('');
+document.writeln();
 document.writeln(says);
 document.writeln(purr);
 document.writeln(name);
@@ -632,7 +632,7 @@ anotherCat.get_name = function () {
   return this.says() + ' ' + this.name + ' ' + this.says()
 };
 
-document.writeln('');
+document.writeln();
 document.writeln(anotherCat.get_name());
 console.log(myMammal); // origional object is not affected
 
@@ -654,7 +654,7 @@ var mammal = function (spec) {
 
 var mammal001 = mammal({name:'Herbert'});
 
-document.writeln('');
+document.writeln();
 document.writeln('Functional Inheritance');
 document.writeln(mammal001.get_name());
 
@@ -705,3 +705,225 @@ var myCoolCat = coolcat({name:'Bixby'})
 var name = myCoolCat.get_name();
 
 document.writeln('Super methods: ' + name);
+
+// ================
+// Parts
+// ================
+
+// This example creates a function that can add simple event
+// processing features to any object.
+// Adds an `on` and `fire` method and a private event registry
+
+var eventuality = function (that) {
+  var registry = {};
+
+  that.fire = function (event) {
+    // Fire an event on an object.  The event can be either a string
+    // containing the name of the event or an object containing
+    // a type property containing the name of the event.  Handlers registered
+    // by the `on` method that match the event name will be invoked
+
+    var array, func, handler, i, type = typeof event === 'string' ? event : event.type;
+    // If an array of handlers exists for this event, then loop through
+    // it and execute the handlers in order.
+    if (registry.hasOwnProperty(type)) {
+      array = registry[type];
+      for (i = 0; i < array.length; i += 1) {
+        handler = array[i];
+        // A handler record contains a method and an optional array
+        // of parameters.  If the method is a name, look up the function
+        func = handler.method;
+        if (typeof func === 'string') {
+          func = this[func];
+        }
+        // Invoke a handler.  If the record contained parameters, then pass
+        // them.  Otherwise, pass the event object.
+        func.apply(this, handler.parameters || [event]);
+      }
+    }
+    return this;
+  };
+
+  that.on = function (type, method, parameters) {
+    // Register an event.  Make a handler record.  Put it in a handler array,
+    // making one if it doesn't yet exist for this type
+
+    var handler = {
+      method: method,
+      parameters: parameters
+    };
+
+    if (registry.hasOwnProperty(type)) {
+      registry[type].push(handler);
+    } else {
+      registry[type] = [handler];
+    }
+    return this;
+  };
+  return that;
+};
+
+var thing1 = {greeting: 'hello world'};
+
+var e = eventuality(thing1);
+e.on('helloWorld', function(){console.log('hello thing1')}, []);
+e.fire('helloWorld');
+
+// =========================================================
+// Arrays
+// =========================================================
+
+var is_array = function (value) {
+  return Object.prototype.toString.apply(value) === '[object Array]';
+};
+
+var firstArray = [1,2,3,4];
+
+console.log(is_array(firstArray));
+
+document.writeln();
+
+var secondArray = [4, 8, 15, 16, 23, 42];
+
+var add = function (a,b) {
+  return a + b;
+};
+
+var mult = function (a,b) {
+  return a * b;
+};
+
+
+var sum = secondArray.reduce(add, 0);
+document.writeln('Array.reduce for sum: ' + sum);
+
+var product = secondArray.reduce(mult, 1);
+document.writeln('Array.reduce for mult: ' + product);
+
+document.writeln();
+document.writeln('Adding a method directly to an array.');
+
+secondArray.total = function () {
+  return this.reduce(add, 0);
+}
+
+document.writeln('.total method on array: ' + secondArray.total());
+
+// Create an array of a certain dimension 
+// with a generic initial value for each element
+Array.dim = function (dimension, initial) {
+  var a = [], i;
+  for (i = 0; i < dimension; i += 1) {
+    a[i] = initial;
+  }
+    return a;
+};
+
+var thirdArray = Array.dim(10, 0);
+
+// Create a two dimensional array with initial values
+Array.matrix = function (m, n, initial) {
+  var a, i, j, mat = [];
+  for (i = 0; i < m; i+= 1) {
+    a = [];
+    for (j = 0; j < n; j += 1) {
+      a[j] = initial;
+    }
+    mat[i] = a;
+  }
+  return mat;
+};
+
+var myMatrix = Array.matrix(4, 4, 0);
+document.writeln(myMatrix[3][3]); // 0
+document.writeln();
+
+// Method to make an identity matrix
+Array.identity = function (n) {
+  var i, mat = Array.matrix(n, n, 0);
+  for (i = 0; i < n; i += 1) {
+    mat[i][i] = 1;
+  }
+  return mat;
+};
+
+myMatrix = Array.identity(4);
+document.writeln(myMatrix[3][3]);
+
+
+// =========================================================
+// Methods
+// =========================================================
+
+// ==============
+// Array Methods
+// ==============
+// Sort Objects by properties
+
+var by = function (name) {
+  return function (o, p) {
+    var a, b;
+    if (typeof o === 'object' && typeof p === 'object' && o && p) {
+      a = o[name];
+      b = p[name];
+      if (a === b) {
+        return 0;
+      }
+      if (typeof a === typeof b) {
+        return a < b ? -1 : 1;
+      }
+      return typeof a < typeof b ? -1 : 1;
+    } else {
+      throw {
+        name: 'Error',
+        message: 'Expected an object when sorting by ' + name
+      };
+    }
+  };
+};
+
+var s = [
+  {first: 'Joe', last: 'Besser'},
+  {first: 'Moe', last: 'Howard'},
+  {first: 'Joe', last: 'DeRita'},
+  {first: 'Shemp', last: 'Howard'},
+  {first: 'Larry', last: 'Fine'},
+  {first: 'Curly', last: 'Howard'}
+];
+
+s.sort(by('first'));
+
+// Chaining sort is not stable- not garunteed to produce correct result when:
+// s.sort(by('first')).sort('last')
+
+// Function takes a member name string and
+// an optional minor comparison function
+// and returns a comparison function that
+// can be used to sort an array of objects
+// that contain that member.  The minor comparison
+// function is used to break ties
+
+var byVersion2 = function (name, minor) {
+  return function (o, p) {
+    var a, b;
+    if (typeof o === 'object' && typeof p === 'object' && o && p) {
+      a = o[name];
+      b = p[name];
+      if (a === b) {
+        return typeof minor === 'function' ? minor(o,p) : 0;
+      }
+      if (typeof a === typeof b) {
+        return a < b ? -1 : 1;
+      }
+      return typeof a < typeof b ? -1 : 1;
+    } else {
+      throw {
+        name: 'Error',
+        message: 'Expected an object when sorting by ' + name
+      };
+    }
+  };
+};
+
+s.sort(byVersion2('last', by('first')));
+
